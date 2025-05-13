5540bc45c1d69495d80ec9499d58919c7d0375b8
HaroldIsHere <fritzbautista100@gmail.com>
2025-05-13 21:35:10 +0800

    initial commit

---
+++ resources/views/User/acc.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="/css/main.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="navigation_top">
+            <img src="/img/Logo.png" class="Logo" alt="Logo Smashkart">
+            <div class="searchbar-container">
+            <img src="/img/Icons/Search.png" class="searchIcon">
+            <input type="text" id="searchbar" placeholder="Looking for something?">
+            </div>
+        </div>   
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="/css/main.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="navigation_top">
+            <img src="/img/Logo.png" class="Logo" alt="Logo Smashkart">
+            <div class="searchbar-container">
+            <img src="/img/Icons/Search.png" class="searchIcon">
+            <input type="text" id="searchbar" placeholder="Looking for something?">
+            </div>
+        </div>
+        <div class="categories_tab">
+        </div>
+        <div class="highlights">
+            <img src="/img/sale1.png" class="Promo1" alt="Promo 1">
+        </div>    
+        <div class="onsale">
+            <h class="title1">ON SALE! 30% OFF</h>
+            <div class="saleProducts">
+                
+            </div>    
+        </div>
+    </body>
+</html>
